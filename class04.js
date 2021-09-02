5aa0caec5e9680cd7fd9920edce2d006e67e7ca6
nitipoom1456 <nitipoom.it@mail.kmutt.ac.th>
2021-09-02 21:55:47 +0700

    cont plz

---
+++ class04.js
@@ -0,0 +1,36 @@
+
+let q1 = {
+    question: "1+1="
+}
+
+let q2 = {
+    question: "1+1=",
+    answer: {
+
+    }
+     
+}
+let q3 = {
+    question: "1+1="
+}
+let q4 = {
+    question: "1+1="
+}
+let q5 = {
+    question: "1+1="
+}
+let player = {
+    name : () ->{
+
+    },
+    answer: () ->{
+
+    }
+    score: () ->{
+        
+    }
+}
+
+
+
+
