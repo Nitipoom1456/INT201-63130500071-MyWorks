0308db1cfe6c0b79f3daa9d1160436d0098811df
nitipoom1456 <nitipoom.it@mail.kmutt.ac.th>
2021-10-28 17:26:06 +0700

    dom2

---
+++ class10.js
@@ -0,0 +1,55 @@
+// const divProductsEle = document.querySelector('#products');
+// // console.log(divProductsEle);
+
+// let product = {
+//     productId: 'P001',
+//     productName: 'iphone',
+//     price: 29990
+// };
+
+// const divProductEle = document.createElement('div');
+
+
+// divProductEle.setAttribute('id', product.productId);
+
+// const pProductIdEle = document.createElement('p');
+// pProductIdEle.textContent = product.productId;
+
+// divProductEle.appendChild(pProductIdEle);
+
+// const pProductNameEle = document.createElement('p');
+// pProductNameEle.textContent = product.productName;
+// divProductEle.appendChild(pProductNameEle);
+
+// const pProductPriceEle = document.createElement('p');
+// pProductPriceEle.textContent = product.price + ` Bath`;
+// divProductEle.appendChild(pProductPriceEle);
+
+
+
+// divProductsEle.appendChild(divProductEle);
+
+import { product1,product2,product3,product4,product5 } from "prod.js";
+
+const showProduct = document.querySelector("#products");
+
+let div1 = document.createElement('div'):
+div1.setAttribute('id', product1.id);
+
+
+
+
+
+let div1 = document.createElement('div'):
+div1.setAttribute('id', product1.id);
+
+
+
+
+
+
+
+
+
+
+
