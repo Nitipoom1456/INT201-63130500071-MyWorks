4fc5548cc4adbf682f09cf943e2d75f6faa127e9
nitipoom1456 <nitipoom.it@mail.kmutt.ac.th>
2021-10-31 00:39:29 +0700

    earphone shop

--- class10.js
+++ class10.js
@@ -29,27 +29,58 @@
 
 // divProductsEle.appendChild(divProductEle);
 
-import { product1,product2,product3,product4,product5 } from "prod.js";
+import {products} from "./prod2.js";
 
-const showProduct = document.querySelector("#products");
+let outerDiv;
+let num = 0;
 
-let div1 = document.createElement('div'):
-div1.setAttribute('id', product1.id);
+products.forEach(product => {
+    if (num == 0) {
+        outerDiv = document.createElement("div");
+        outerDiv.setAttribute("class", "row row-cols-2 row-cols-lg-4 g-2 g-lg-3");
+        num = num + 1;
+    }
+    let img = document.createElement("img");
+    img.src = `${product.img}`;
+    img.height = "250";
+    img.style = "text-align:center;";
+    img.className = "img-fluid";
 
+    const innerDiv = document.createElement("div");
+    innerDiv.setAttribute("class", "col bg-body card rounded p-2 bd-highlight mh-100 px-md-5 py-md-5 mx-md-5 my-md-5");
+    innerDiv.appendChild(img);
 
+    const content = document.createElement("div");
 
+    content.appendChild(document.createElement("br"));
+    let pName = document.createElement("h5");
+    pName.textContent = product.name;
+    content.appendChild(pName);
+    content.appendChild(document.createElement("br"));
+    content.appendChild(document.createTextNode(`ID: ${product.id}`));
+    content.appendChild(document.createElement("br"));
+    content.appendChild(document.createTextNode(`${product.desc}`));
+    content.appendChild(document.createElement("br"));
+    content.appendChild(document.createTextNode(`Price: ${product.price} BATH`));
+    content.appendChild(document.createElement("br"));
+    content.appendChild(document.createTextNode(`Stock: ${product.stock}`))
+    content.appendChild(document.createElement("br"));
 
+    let btn = document.createElement("a");
+    if (product.stock === 0){
+        btn.setAttribute("class", "btn btn-danger mw-100 cursor-not-allowed");
+        btn.setAttribute("style", "cursor: not-allowed");
+        btn.appendChild(document.createTextNode("Out of stock"))
+    } else{
+        btn.setAttribute("class", "btn btn-primary mw-100");
+        btn.setAttribute("href", "https://www.google.com");
+        btn.setAttribute("target", "_blank");
+        btn.appendChild(document.createTextNode("Add to cart"))
+    }
+    content.appendChild(btn);
+    innerDiv.appendChild(content);
+    outerDiv.appendChild(innerDiv);
+});
 
-let div1 = document.createElement('div'):
-div1.setAttribute('id', product1.id);
-
-
-
-
-
-
-
-
-
-
+document.querySelector("#products").appendChild(outerDiv);
 
